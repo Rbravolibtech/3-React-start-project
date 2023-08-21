8af22af9160eeb3e669da0ae9146f40e59d8e9db
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-21 16:31:48 -0600

    47 react JSX expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+	const expenseDate = new Date(2023, 8, 21);
+	const expenseTitle = "Car Insurance";
+	const expenseAmount = "500.00";
+
 	return (
 		<div className="expense-item">
-			<div>March 28th 2021</div>
+			<div>{expenseDate.toISOString()}</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
-				<div className="expense-item__price">$500.00</div>
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price">${expenseAmount}</div>
 			</div>
 		</div>
 	);
